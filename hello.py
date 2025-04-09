2333296ad9b90976a5ce270c37d57e920d92b8cc
B-Gerb <gerber.br@northeastern.edu>
2025-04-09 17:57:10 -0400

    Using multiple thresholds

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ import pandas as pd
 import plotly.express as px
 
 
+
 text("# Welcome to Preswald!")
 text("This is your first app. 🎉")
 # Load the CSV
@@ -16,12 +17,18 @@ sql = "SELECT * FROM pistachio_csv WHERE AREA > 75000"
 filtered_df = query(sql, "pistachio_csv")
 
 text("## Data Overview")
-threshold = slider("Threshold", min_val=0, max_val=150000, default=75000)
+threshold = slider("Threshold For Area", min_val=0, max_val=150000, default=75000)
 table(df[df["AREA"] > threshold], title="Dynamic Data View")
 
 
+thershold_area_df = df[df["AREA"] > threshold]
 
-fig = px.scatter(df[df["AREA"] > threshold], x="AREA", y="PERIMETER", color="Class")
-plotly(fig)
+fig1 = px.scatter(thershold_area_df, x="AREA", y="PERIMETER", color="Class")
+plotly(fig1)
 
-text("## Data Overview")
+threshold1 = slider("Threshold For ECCENTRICITY", min_val=0, max_val=100, default=50)
+threshold1 /= 100 # Convert to a percentage
+threshold_eccentricity_df = thershold_area_df[thershold_area_df["ECCENTRICITY"] > threshold1]
+fig2 = px.histogram(threshold_eccentricity_df, x="ECCENTRICITY", color="Class", barmode="group")
+plotly(fig2)
+
