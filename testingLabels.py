9e948e5058d7b8df790aa3fe1fc2e78baf10b031
B-Gerb <gerber.br@northeastern.edu>
2025-04-10 19:38:35 -0400

    running some testing

---
+++ testingLabels.py
@@ -0,0 +1,6 @@
+from preswald import  connect, slider
+connect() 
+threshold = slider("Threshold For Area", min_val=0, max_val=150000, default=75000)
+
+threshold1 = slider("Threshold For Area", min_val=1000, max_val=150000, default=75000)
+
